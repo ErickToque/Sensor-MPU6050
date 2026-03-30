4256c728914bf61bae8b1c0504b39fb141631d62
Erick Toque <142357110+ErickToque@users.noreply.github.com>
2026-03-29 22:16:42 -0500

    Update print statements for clarity in MPU6050 script

--- Python/MPU6050_Exportar.py
+++ Python/MPU6050_Exportar.py
@@ -6,11 +6,11 @@ import keyboard
 import matplotlib.pyplot as plt
 
 # --- CONFIGURACIÓN ---
-PORT = "COM5"
-BAUD = 9600
+PORT = "COM5" #ACTUALICE
+BAUD = 9600 #ACTUALICE
 
 # Carpeta donde quieres guardar
-OUTPUT_DIR = r"E:\PUCP\Biomecatronica\Sensor_MPU6050\Datos_MPU6050"
+OUTPUT_DIR = r"E:\PUCP\Biomecatronica\Sensor_MPU6050\Datos_MPU6050" #ACTUALICE
 os.makedirs(OUTPUT_DIR, exist_ok=True)
  
 # Nombre del archivo final con timestamp
@@ -21,8 +21,8 @@ OUTPUT_FILE = os.path.join(OUTPUT_DIR, f"mpu_datos_{timestamp}.csv")
 ser = serial.Serial(PORT, BAUD, timeout=1)
 time.sleep(2)
 
-print("📡 Capturando datos del MPU6050...")
-print("👉 Presiona '1', '2' o '3' para cambiar de clase. Presiona 'ESPACIO' para terminar.")
+print("Capturando datos del MPU6050...")
+print("Presiona '1', '2' o '3' para cambiar de clase. Presiona 'ESPACIO' para terminar.") #IMPORTANTE PARA ETIQUETADO
 
 # --- LISTA PARA GUARDAR DATOS ---
 data = []
@@ -50,24 +50,24 @@ x_vals, ax_vals, ay_vals, az_vals, class_vals = [], [], [], [], []
 # --- LOOP ---
 while True:
     if keyboard.is_pressed("space"):  # terminar
-        print("⏹ Captura finalizada por el usuario.")
+        print("Captura finalizada por el usuario.")
         break
     elif keyboard.is_pressed("1"):
         current_class = "Clase 1"
         class_numeric = 1
-        print("🔖 Clase cambiada a 1")
+        print("Clase cambiada a 1")
         class_lines.append(len(x_vals))
         time.sleep(0.3)
     elif keyboard.is_pressed("2"):
         current_class = "Clase 2"
         class_numeric = 2
-        print("🔖 Clase cambiada a 2")
+        print("Clase cambiada a 2")
         class_lines.append(len(x_vals))
         time.sleep(0.3)
     elif keyboard.is_pressed("3"):
         current_class = "Clase 3"
         class_numeric = 3
-        print("🔖 Clase cambiada a 3")
+        print("Clase cambiada a 3")
         class_lines.append(len(x_vals))
         time.sleep(0.3)
 
@@ -121,7 +121,7 @@ df = pd.DataFrame(data, columns=["Tiempo[ms]", "Ax", "Ay", "Az", "Gx", "Gy", "Gz
 
 # --- GUARDAR CSV ---
 df.to_csv(OUTPUT_FILE, index=False)
-print(f"✅ Datos guardados en {OUTPUT_FILE}")
+print(f"Datos guardados en {OUTPUT_FILE}")
 
 # --- GUARDAR FIGURA ---
 FIG_FILE = os.path.splitext(OUTPUT_FILE)[0] + ".png"
